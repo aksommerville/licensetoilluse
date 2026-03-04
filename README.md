74656aa0a3e208235e384da5e2549473b534decb
andy <aksommerville@gmail.com>
2026-03-03 19:07:25 -0500

    Fresh project and some starter graphics.

---
+++ README.md
@@ -0,0 +1,7 @@
+# License to Illuse
+
+Requires [Egg v2](https://github.com/aksommerville/egg2) to build.
+
+Uplifting Game Jam #7, theme "ILLUSION".
+
+
---
+++ src/game/licensetoilluse.h
@@ -0,0 +1,19 @@
+#ifndef EGG_GAME_MAIN_H
+#define EGG_GAME_MAIN_H
+
+#include "egg/egg.h"
+#include "util/stdlib/egg-stdlib.h"
+#include "util/graf/graf.h"
+#include "util/font/font.h"
+#include "util/res/res.h"
+#include "util/text/text.h"
+#include "egg_res_toc.h"
+#include "shared_symbols.h"
+
+extern struct g {
+  void *rom;
+  int romc;
+  struct graf graf;
+} g;
+
+#endif
